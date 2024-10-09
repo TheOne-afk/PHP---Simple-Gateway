4bd304743d45e6aea4c261daca84915f7c481290
TheOne-afk <156886567+TheOne-afk@users.noreply.github.com>
2024-10-09 20:39:48 +0800

    UPDATE - 10/9/2024 8:39 PM
    Addded the updated database sql

--- matt/db/simplelogindb.sql
+++ matt/db/simplelogindb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2024 at 02:23 PM
+-- Generation Time: Oct 09, 2024 at 02:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -86,8 +86,32 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `create_at`, `role`, `activation_token`, `begin`, `attempt`, `locked`) VALUES
-(76, 'Ma.Ma', 'matthewdelacruz647@gmail.com', '$2y$10$Hv4atNOkiX6fi8m5e00CYexy7Whys0lg6THE6T/7YFf2BqnxC56lm', '2024-10-03 13:21:25', 'user', NULL, NULL, 0, 0),
-(77, 'user.userOne', 'user01@user.com', '$2y$10$IMpnFM4vukPtGs51DQlohuaAYnOAQE4zIq0PpCn6vsIrPNPqukjPy', '2024-10-03 13:07:36', 'user', 'NULL', '2024-10-03 14:34:50', 6, 1);
+(76, 'Ma.Ma', 'matthewdelacruz647@gmail.com', '$2y$10$Hv4atNOkiX6fi8m5e00CYexy7Whys0lg6THE6T/7YFf2BqnxC56lm', '2024-10-04 15:04:38', 'admin', NULL, NULL, 0, 0),
+(78, 'user.one', 'user01@user.com', '$2y$10$iV5aSj8ornZdubnOr5bat.6.3MYmHI8OfAEnVZcQ2XHwkPv0rlNUq', '2024-10-09 07:29:03', 'user', 'd7da6647697d516e377ebeb78c57379563eb33a4f94d755de229902b94cf8a60', NULL, 0, 0),
+(79, 'user.two', 'user02@user.com', '$2y$10$sudRmz74NgRbOHCQD/awS.qDn2RjiMqCkZKfvKpdjIsVxN0Xkan3i', '2024-10-09 07:29:31', 'user', '53af502d6213947f0a288daeaf5c24a880711ef08698a1ba6757da5978964d84', NULL, 0, 0),
+(80, 'user.three', 'user03@user.com', '$2y$10$v5q7csXNNJq5LV9IZ1p3H.h13jMeCw4pcrK0b22AuBoG9PyrSHVeu', '2024-10-09 07:29:56', 'user', 'f9c5b9fe39e2b109e9f858dd1a7824b575583485b627b49827650d4ed1f707dc', NULL, 0, 0),
+(81, 'user.four', 'user04@user.comm', '$2y$10$wScXfnW01mzwiMF7FZ/kWewDwQbd66xGOql//C6JxVzlSGCRr46si', '2024-10-09 09:24:22', 'user', '4a0d65c5868eaefd9dc010bbbf6e4415da5649842a50dee5761fbad0b4d21125', NULL, 0, 0),
+(82, 'user.five', 'user05@user.com', '$2y$10$SIuHXN/e3zUWqVsfTzJjEuOPvX5kIicFmFlzGQVmhten9FwxErYJO', '2024-10-09 07:30:46', 'user', 'ac88b93a758d09820e71b95cdf41904824f7bbe519f92025d7aa16bb8423f650', NULL, 0, 0),
+(83, 'user.six', 'user06@user.com', '$2y$10$gYW7FfhFA72wSdoixnh75.DO9.D5i5DUyE9fwZtQO.3HKHqQAPUrm', '2024-10-09 07:31:15', 'user', 'a962168925b4425c4084eb8fb7e15e3e521aa002d01d776ee75d6d7e35788d09', NULL, 0, 0),
+(84, 'user.seven', 'user07@user.com', '$2y$10$OidybLEz6ysPMqldxQiydOjMQDeNIS1byR91dFIO6THkhabfgK7fy', '2024-10-09 07:31:29', 'user', '8c7ca2715304a4aeef2b59123ab005bdb761900c490742880bb9e7999f6d1ff8', NULL, 0, 0),
+(85, 'user.eight', 'user08@user.com', '$2y$10$358x.G1H4t45ozsZYfHG6eiETpp1s2JBN6mN72/5tAc73XEm3ZAPi', '2024-10-09 07:31:46', 'user', '8c46a3745b2e8db187d4e054f9a54d441f931ad515cd43123400a470973891b6', NULL, 0, 0),
+(86, 'user.nine', 'user09@user.com', '$2y$10$cvmjZ8vFppUahOEQUvSNke3u7b0LnrGe1lubGz1Ff1VpmNY2P5dIO', '2024-10-09 07:31:59', 'user', 'dcc278542aa907c5ec245bdbe37ce5238f21a4547e7b0ab2ca1df8881c84f1e1', NULL, 0, 0),
+(88, 'user.eleven', 'user11@user.com', '$2y$10$GwKPOHh8O53gWfiVvWFedeFRdF7yAFjPxUxVLH1owlOokp39IXHyK', '2024-10-09 07:32:27', 'user', 'a54b8792995efec91f62825f76e6e182d63d529154afa27fb6bade93a21dbd3f', NULL, 0, 0),
+(89, 'user13', 'user13@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(90, 'user14', 'user14@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(91, 'user15', 'user15@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(92, 'user16', 'user16@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(93, 'user17', 'user17@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(94, 'user18', 'user18@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(95, 'user19', 'user19@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(96, 'user20', 'user20@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(99, 'user23', 'user23@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(100, 'user24', 'user24@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(101, 'user25', 'user25@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(102, 'user26', 'user26@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(103, 'user27', 'user27@example.com', 'password123', '2024-10-09 07:35:11', NULL, NULL, NULL, NULL, 0),
+(104, 'user28', 'user28@example.comm', 'password123', '2024-10-09 09:49:07', '', NULL, NULL, 0, 0),
+(105, 'user29', 'user29@example.com', 'password123', '2024-10-09 11:05:48', '', NULL, NULL, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -134,7 +158,7 @@ ALTER TABLE `login_attempt`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
